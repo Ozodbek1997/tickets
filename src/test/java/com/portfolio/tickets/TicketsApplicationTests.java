8f4a8618134cc98b1bd15f13ae35c823cd3e65a4
Ozodbek <ozodbek.xomidbekov@gmail.com>
2025-12-01 21:10:00 +0900

    Initial commit

---
+++ src/test/java/com/portfolio/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
